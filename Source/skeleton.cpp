ae70625e9f52af1414201748d88299791cc396ba
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-14 13:11:15 +0100

    Much larger

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -58,7 +58,7 @@ struct Intersection {
   int triangle_index;
 };
 float focal_length = 100.0;
-vec4  camera_position(0.0, 0.0, -3.0, 1.0);
+vec4  camera_position(0.0, 0.0, -1.0, 1.0);
 cl_float3  cl_camera_position  = {0.0, 0.0, -3.0};
 
 float pitch = 0.0f;
