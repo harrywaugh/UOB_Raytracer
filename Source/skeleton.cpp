69f36adf5b7e84c108194fd6d4b2e774cdfe97d3
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-16 20:53:09 +0100

    Asjusting focal for larger image

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -58,7 +58,7 @@ struct Intersection {
   float distance;
   int triangle_index;
 };
-float focal_length = 1500.0;
+float focal_length = 300.0;
 vec4  camera_position(0.0, 0.0, -2.5, 1.0);
 cl_float3  cl_camera_position  = {0.0, 0.0, -1.5};
 
