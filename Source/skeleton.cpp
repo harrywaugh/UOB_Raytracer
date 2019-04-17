0646822404f65de11ec5ba364ee412ca3c83ba8e
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-17 08:27:38 +0100

    Adjusted cam pos

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -58,7 +58,7 @@ struct Intersection {
   float distance;
   int triangle_index;
 };
-float focal_length = 3000.0;
+float focal_length = 2500.0;
 vec4  camera_position(0.0, 0.0, -2.5, 1.0);
 cl_float3  cl_camera_position  = {0.0, 0.0, -2.7};
 
